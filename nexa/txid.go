3df71a345e626e9975c3ec35a52e770ace6236dc
HeHuiqi <1710308677@qq.com>
2023-04-18 17:00:24 +0800

    add one input test

--- nexa/txid.go
+++ nexa/txid.go
@@ -1,5 +1,7 @@
 package nexa
 
+import "encoding/hex"
+
 /*
 
 "txid": "4a5733d194cd9572937b5ef766c35c631301430251f42d24ab343ec150478481",
@@ -67,4 +69,16 @@ func TxID() {
 
 	txId := TxDoubleHash256(txIdHex)
 	println("txId:", txId)
+
+	pretxId := "1848efbaee543dd058d3529fcfe95652e149dcff02f820245426703c1c65a975"
+	// pretxId = "3c6746ede34e13dce746c049ca7ec53ce63d95bb8b9cfa43e7d8f8e1ae4501de"
+	txidReverse := HashReverseHex(pretxId)
+	ouputIndexHex := TxAmountToLitteEndianHex(1)
+	println("ouputIndexHex:", ouputIndexHex)
+	preout := hex.EncodeToString(txidReverse) + ouputIndexHex
+	// preout = pretxId + ouputIndexHex
+	println("preout:", preout)
+	// 16af7506d867a4f24da79ad4f70fc82bb4d3d9a79502e1f603d6096e25f0e8f2
+	res := TxDoubleHash256(preout)
+	println("res:", res)
 }
